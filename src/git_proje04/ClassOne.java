0872be2a5f7c3b51ba155a0d52e43cc4972ec17e
ishaksengul <67099229+ishaksengul@users.noreply.github.com>
2020-07-05 20:50:18 +0200

    Update ClassOne.java
    ilk commit from feature branche

--- src/git_proje04/ClassOne.java
+++ src/git_proje04/ClassOne.java
@@ -4,6 +4,7 @@ public class ClassOne {
 
 	public static void main(String[] args) {
 		System.out.println("Ilk commit local masterdan");
+		System.out.println("Ilk commit local masterdan");
 	}
 
 }
